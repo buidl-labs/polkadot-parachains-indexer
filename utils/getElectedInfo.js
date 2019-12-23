c351b51c5706799becfce18856b4dc453600f659
Bhaskar Singh <allstarbhaskarkumar@gmail.com>
2019-12-23 22:08:59 +0530

    fix: EI is not defined reference error

--- utils/getElectedInfo.js
+++ utils/getElectedInfo.js
@@ -1,5 +1,5 @@
 const { ApiPromise, WsProvider } = require("@polkadot/api");
-
+const EI = require("../models/ElectedInfo");
 async function getElectedInfo(){
     const wsProvider = new WsProvider("wss://kusama-rpc.polkadot.io");
     const api = await ApiPromise.create({ provider: wsProvider });
