8c4f1968641420e03e90172e3014799207599e6d
Bhaskar Singh <allstarbhaskarkumar@gmail.com>
2020-01-02 01:08:33 +0530

    get all data in one go

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const EI = require("./models/ElectedInfo");
 const Intention = require("./models/Intention");
 const Sentry = require('@sentry/node');
 const https = require("https");
+const { hexToString } = require("@polkadot/util");
+
 
 if (!config.get('DB')) {
     throw new Error('Database url is required!');
@@ -36,6 +38,124 @@ mongoose.connect(configDB, {useNewUrlParser: true,  useUnifiedTopology: true})
 .then(() => console.log("connected to database"))
 .catch(() => console.log("database failed to connect"));
 
+const createApi = async () => {
+    // console.log(`Connecting to API...`);
+    let electedInfo;
+    const wsProvider = new WsProvider("wss://kusama-rpc.polkadot.io");
+    const api = await ApiPromise.create({ provider: wsProvider });
+    await api.isReady;
+    // console.log(`API is ready`);
+    // console.clear();
+    // Fetch recent reward events from Polkascan
+    const res = await fetch(
+      "https://polkascan.io/kusama-cc3/api/v1/event?&filter[module_id]=staking&filter[event_id]=Reward&page[size]=10"
+    );
+    const json = await res.json();
+    const rewardData = await json.data;
+  
+    // Retrieve currentElected validators for current block
+    const currentValidators = await api.query.staking.currentElected();
+    // Retrieve all validators
+    const allValidators = await api.query.staking.validators();
+    // Parse validators
+    const parsedValidators = JSON.parse(JSON.stringify(allValidators))[0];
+    // Retrieve session validators
+    const sessionValidators = await api.query.session.validators();
+    const intentions = await parsedValidators.filter(
+      validator => !sessionValidators.includes(validator)
+    );
+    const validatorsAndIntentions = [...sessionValidators, ...intentions];
+    // Retrieve the last known era reward
+    const reward = await rewardData[0].attributes.attributes[0].value;
+    // Retrieve the hashes of the end of era blocks
+    const hash = await Promise.all(
+      rewardData.map(data =>
+        api.rpc.chain.getBlockHash(data.attributes.block_id - 1)
+      )
+    );
+    // Retrieve the era points for all end of era blocks
+    const eraPoints = await Promise.all(
+      hash.map(data =>
+        api.query.staking.currentEraPointsEarned.at(`${data.toString()}`)
+      )
+    );
+    // Retrieve an array of the list of all elected validators at the end of era blocks
+    const validatorList = await Promise.all(
+      hash.map(data => api.query.staking.currentElected.at(`${data.toString()}`))
+    );
+  
+    let result = {};
+  
+    await Promise.all(
+      validatorList.map(async validator => {
+        await Promise.all(
+          validator.map(async address => {
+            const commission = await api.query.staking.validators(address);
+            const name = await api.query.nicks.nameOf(`${address.toString()}`);
+            result[address] = {
+              stashId: address.toString(),
+              stashIdTruncated: `${address
+                .toString()
+                .slice(0, 4)}...${address.toString().slice(-6, -1)}`,
+              points: [],
+              poolReward: "",
+              totalStake: "",
+              commission: commission[0].commission.toNumber() / 10 ** 7,
+              name: name.raw[0]
+                ? hexToString(name.raw[0].toString())
+                : `Validator (...${address.toString().slice(-6, -1)})`
+            };
+          })
+        );
+      })
+    );
+  
+    eraPoints.map((eraPoint, index) => {
+      eraPoint.individual.map((point, validatorIndex) => {
+        result[validatorList[index][validatorIndex]].points.push(
+          point.toNumber() / eraPoint.total.toNumber()
+        );
+        return 0;
+      });
+      return 0;
+    });
+  
+    const validatorData = await Promise.all(
+      Object.keys(result).map(async (key, index) => {
+        const validatorPoolReward =
+          ((result[key].points.reduce((acc, curr) => acc + curr, 0) /
+            result[key].points.length) *
+            reward) /
+          10 ** 12;
+        electedInfo = await api.derive.staking.electedInfo();
+        const stakeInfo = await api.derive.staking.account(key.toString());
+        const totalStake =
+          stakeInfo !== undefined
+            ? stakeInfo.stakers.total.toString() / 10 ** 12
+            : undefined;
+        result[key].totalStake = totalStake;
+        result[key].poolReward = isNaN(validatorPoolReward)
+          ? "Not enough data"
+          : (1 - result[key].commission / 100) * validatorPoolReward;
+        //   setElectedInfo(electedInfo);
+        return result[key];
+      })
+    );
+    const filteredValidatorData = validatorData.filter(curr =>
+      currentValidators.includes(curr.stashId)
+    );
+    // setApiConnected(true);
+    // setValidatorData(filteredValidatorData);
+    // setIntentionData(intentions);
+    // setValidatorsAndIntentions(validatorsAndIntentions);
+    return {
+      filteredValidatorData,
+      intentions,
+      validatorsAndIntentions,
+      electedInfo
+    };
+  };
+
 /**
  * Remove all old validators data
  * and fetch fresh validators data
@@ -43,15 +163,32 @@ mongoose.connect(configDB, {useNewUrlParser: true,  useUnifiedTopology: true})
  */
 eraChange.on("newEra", async () => {
     try{
-        let result = {};
+        let final = {};
         await Validator.deleteMany({});
         await EI.deleteMany({});
         await Intention.deleteMany({});
-        let validators = await filteredValidatorData();
-        result.filteredValidatorsList = await Validator.insertMany(JSON.parse(JSON.stringify(validators)));
-        result.electedInfo = await getElectedInfo();
-        result.intentionsData = await getValidatorsAndIntentions();
-        io.emit('onDataChange', result);
+        const result = await createApi();
+
+        const savedValidator = await Validator.insertMany(
+          JSON.parse(JSON.stringify(result.filteredValidatorData))
+        );
+    
+        const electedInfoData = new EI(
+          JSON.parse(JSON.stringify(result.electedInfo))
+        );
+        const savedElectedInfo = await electedInfoData.save();
+    
+        const intentionData = new Intention({
+          intentions: JSON.parse(JSON.stringify(result.intentions)),
+          validatorsAndIntentions: JSON.parse(
+            JSON.stringify(result.validatorsAndIntentions)
+          )
+        });
+        const savedIntention = await intentionData.save();
+        final.filteredValidatorsList = savedValidator
+        final.electedInfo = savedElectedInfo
+        final.intentionsData = savedIntention;
+        io.emit('onDataChange', final);
     }catch(err){
         console.log(err);
     }
