f0b64074f5ee9daebe11e0fd97d59c2ce567de5b
Bhaskar Singh <allstarbhaskarkumar@gmail.com>
2020-01-18 15:11:27 +0530

    change era when eraprogress equals to 1

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ eraChange.on('newEra', async () => {
   await api.derive.session.info(header => {
     const eraProgress = header.eraProgress.toString();
     // console.log(eraLength,eraProgress,sessionLength,sessionProgress)
-    if (parseInt(eraProgress) === 40) {
+    if (parseInt(eraProgress) === 1) {
       Sentry.captureMessage(`Era changed at: ${new Date()}`);
       eraChange.emit('newEra');
     }
