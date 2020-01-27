42b7e59b56ac9eb193bd074378ce0459cdf69d0c
Bhaskar Singh <allstarbhaskarkumar@gmail.com>
2020-01-27 20:16:43 +0530

    update intentions endpoint to store intentions info data too

--- routes/intentions.js
+++ routes/intentions.js
@@ -4,7 +4,7 @@ const Intention = require('../models/Intention');
 router.get('/intentions', async (req, res) => {
   try {
     const result = await Intention.find();
-    res.json({intentions: result[0].intentions})
+    res.json({ intentions: result[0].intentions, info: result[0].info });
   } catch (err) {
     res.status(400).send({ err: 'Error Bro', err: err });
   }
